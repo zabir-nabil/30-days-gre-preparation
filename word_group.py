4fb8d37d736660c5d04ddefcccdba9e4a5b3e113
Zabir Al Nazi Nabil <zabiralnazi@yahoo.com>
2022-09-26 11:28:25 +0600

    added

---
+++ word_group.py
@@ -0,0 +1,87 @@
+# dictionary containing the word groups
+"""
+word_world = {
+    "word_group1": 
+    [
+        "word1": {
+            "meaning": ["meaning 1", "specific meaning 2"],
+            "use": ["sentence 1", "sentence 2"],
+            "mnemonic": ["mnemonic 1", "mnemonic 2"]
+        }
+    ]
+}
+
+word_world_without_groups = {
+    "word_1" : [
+        ("meaning 1", "specific meaning 2"),
+        ("tricky synonym 1", "tricky synonym 2"), # not necessarily hard ones, just tricky
+        ("sentence 1", "sentence 2"),
+        ("mnemonic 1", "mnemonic 2"),
+        ("word tag / probable word group"),
+        ("other word 1 which should be studied with this word", "other word 2 which should be studied with this word")
+    ]
+}
+"""
+"""
+rough notes:
+exonerate - https://mammothmemory.net/english/5000-new-words/vocabulary/word-list/i/exonerate.html
+turgid - tar eto jid je jide fule ase juicy turkey murgi
+turpitude - tarpana - if your intention is to tarpano someone it's ill, torpedo
+https://www.merriam-webster.com/words-at-play/torturous-vs-tortuous-usage
+tendentious - Tend or Tendency + ious or bias. So, TENDENTIOUS is a TENDENcy towards one viewpoint over another.
+calumny - call alumny and bodnam kora
+exonerate - exoner > prisoner > ex prisoner > acquit
+apprise - apprise of appraisal
+anachronism - ana (against prefix), chronism (time) -> outdated
+impassive - opposite of expressive
+"""
+word_world_without_groups = {
+    "mollify": [
+        ("appease", "pacify", "alleviate"),
+        ("humour", ),
+        ("NGOs Criticize Amazon's Plan to Mollify EU Regulators.", "Inflation is slowing, U.S. August CPI to show, but not enough to mollify a worried Fed"),
+        ("mollify -> molest | nullify, nullify [cancel out/reduce the impact] the after-molest impact. If someone is violated he/she should be mollified."),
+        ("make feel better / make things better [from worse]"),
+        ("nullify")
+    ],
+    "limpid": [
+        ("transparent", "lucid", "easy to understand", "clear (liquid)"),
+        ("vivid"),
+        ("RK/RKay Is a Mystery Drama with Astutely Limpid Answers", "The Railway Children review - kids' adventure has lost none of its limpid charm", "As is so often the case, the publisher shows no idea what this book is about, even what exactly is going on, for the jacket rhetoric gives the briefest mentions of the two main characters and says the book is “told in the tradition of W.G. Sebald.” It’s actually nothing like the novels of Sebald, whose style was slow and deliberate, but limpid as distilled water. What we have here is the voice of one trying to navigate, endure, rise above, and somehow pacify a tapestry of cruelty and grief, while it struggles to find the words and voice that will do the work."),
+        ("limb id (identification), so transparent that every limb can be id'ed through the dress"),
+        ("clear / easy to understand"),
+        ()
+    ],
+    "intransigent": [
+        ("uncompromising", "inflexible"),
+        ("pig-headed", "bloody-minded", "stiff-necked", "resolute"),
+        ("Rushdie Attack Highlights Iran's Intransigence", " In the north, mainly ethnic Serb citizens live in limbo [in a forgotten or ignored place, state, or situation,  in an uncertain or undecided state or condition. After graduating from college, he was in limbo for a while, trying to decide what to do next.], paralyzed by the region's intransigent politics."),
+        ("I have a sergent friend. But when I'm in transit, my sergent friend is inflexible. But when not in transit [remove in], he is transigent."),
+        ("tough / stubborn"),
+        ("transigent")
+    ],
+    "glib": [
+        ("smooth", "fluent but shallow", "oversimplified"),
+        ("superficial"),
+        ("Nicola Sturgeon cannot get away with 'glib' answers at Fringe event, says campaign group", "If Prof. Gyampo Ever Lived in Kumasi, He Would Not Be So Glib and Flip [flippant]"),
+        ("glob is a famous file searching library with any pattern. It can search and list the files fluently but it only gives the filepaths and not the actual files (shallow)."),
+        ("reasonable / useful / fluent but shallow"),
+        ()
+    ],
+    "fawn": [
+        ("baby deer", "flattery to gain favour", "light grey-brown color / fawn-colored"),
+        ("grovel"),
+        ("I hate waiters who fawn over you.", "Students fawn over animals at UPC petting zoo to get their attention.", "CNN, MSNBC, ABC fawn over Liz Cheney after primary loss, blame 'broken' GOP and 'cult of Trump'"),
+        ("FA(win)N - Winner of best FAN award"),
+        ("flattery"),
+        ("fawn over")
+
+    ],
+    "exacerbate": [
+        ("worsen"),
+
+    ]
+}
+word_world = {
+
+}
